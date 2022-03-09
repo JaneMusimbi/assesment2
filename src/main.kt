ee804680b24484950f0bcecbdac54d2d36e9232b
JaneMusimbi <janemusimbi@gmail.com>
2022-03-09 17:05:14 -0500

    assesment

---
+++ src/main.kt
@@ -0,0 +1,41 @@
+fun main(){
+    println(7%3)
+    println("Hi my name is jane and am 25 years old")
+
+
+
+
+
+
+
+}
+fun name(){
+    var y="codeHive"
+    println(y[5])
+    println(y[6])
+    println(y[7])
+    println(y[8])
+
+
+}
+fun modulus(number1:Int,number2:Int):Int{
+    var y=(number1%number2)
+        return y
+
+
+}
+fun parameter(name:String, age:Int):String{
+    var statement=("Hi my name is jane and am 25 years old")
+    return(statement)
+}
+fun leght(){
+    var m="Musimbi"
+    println(m.length)
+}
+
+
+
+
+
+
+
